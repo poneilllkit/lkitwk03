78343a6548480b9817bca5f29445ca1dfb76ce01
poneilllkit <l00170246@student.lyit.ie>
2021-09-28 14:40:26 +0000

    Initial commit

---
+++ Week03/selection_demo/sample_if.py
@@ -0,0 +1,14 @@
+"""
+# File          : sample_if.py
+# Created       : 28.09.2021
+# Author        : P. O'Neill
+# Version       : v1.0.0
+# Licencing     : (C) 2021 Pierce O'Neill, LYIT
+#               Available under GNU Public License (GPL)
+# Description     :
+#
+"""
+
+if __name__ == '__main__':
+    grade = 50
+    if grade >= 40: print('you passed')
