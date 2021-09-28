f67aea0fb9ae6f105efd1f651a2e8c553214cdab
poneilllkit <l00170246@student.lyit.ie>
2021-09-28 18:30:33 +0000

    adding while_loop1.py

---
+++ Week03/loop_demo/loop2.py
@@ -0,0 +1,21 @@
+"""
+# File          : loop2.py
+# Created       : 28.09.2021
+# Author        : P. O'Neill
+# Version       : v1.0.0
+# Licencing     : (C) 2021 Pierce O'Neill, LYIT
+#               Available under GNU Public License (GPL)
+# Description     :
+#
+"""
+
+if __name__ == '__main__':
+    for i in range(10, 5, -1):
+        print(1)
+        # Do include the starting number (10), dom't include the end (5)
+    print ("\n")
+
+    for i in range(0, 12, 3):
+        print(1)
+
+        # Count in 3s
---
+++ Week03/loop_demo/while_loop1.py
@@ -0,0 +1,30 @@
+"""
+# File          : while_loop1.py
+# Created       : 28.09.2021
+# Author        : P. O'Neill
+# Version       : v1.0.0
+# Licencing     : (C) 2021 Pierce O'Neill, LYIT
+#               Available under GNU Public License (GPL)
+# Description     :
+#
+"""
+if __name__ == '__main__':
+    # sample while loop
+    # max = 5
+    # counter = 0
+    # total = 0
+    # while counter <= max:
+    #     total += 9.99
+    #     counter += 1
+    # print("The final amount is: {0:5.2f}".format(total))
+
+    # While true sample
+    text = ""
+    while 1:
+        print("Enter Name")
+        uname = input()
+        if (uname == "joe"):
+            break
+        print("Finished")
+
+
